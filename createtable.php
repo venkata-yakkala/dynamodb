c7deb30f821c0824367fd16a69b6a72802daf957
venkata-yakkala <venkata.yakkala@gmail.com>
2018-02-16 22:41:21 +0530

    Create createtable.php

---
+++ createtable.php
@@ -0,0 +1,55 @@
+<?php
+require 'vendor/autoload.php';
+
+date_default_timezone_set('UTC');
+
+use Aws\DynamoDb\Exception\DynamoDbException;
+
+$sdk = new Aws\Sdk([
+    'endpoint'   => 'http://localhost:8000',
+    'region'   => 'us-west-2',
+    'version'  => 'latest'
+]);
+
+$dynamodb = $sdk->createDynamoDb();
+
+$params = [
+    'TableName' => 'Movies',
+    'KeySchema' => [
+        [
+            'AttributeName' => 'year',
+            'KeyType' => 'HASH'  //Partition key
+        ],
+        [
+            'AttributeName' => 'title',
+            'KeyType' => 'RANGE'  //Sort key
+        ]
+    ],
+    'AttributeDefinitions' => [
+        [
+            'AttributeName' => 'year',
+            'AttributeType' => 'N'
+        ],
+        [
+            'AttributeName' => 'title',
+            'AttributeType' => 'S'
+        ],
+
+    ],
+    'ProvisionedThroughput' => [
+        'ReadCapacityUnits' => 10,
+        'WriteCapacityUnits' => 10
+    ]
+];
+
+try {
+    $result = $dynamodb->createTable($params);
+    echo 'Created table.  Status: ' . 
+        $result['TableDescription']['TableStatus'] ."\n";
+
+} catch (DynamoDbException $e) {
+    echo "Unable to create table:\n";
+    echo $e->getMessage() . "\n";
+}
+
+?>
